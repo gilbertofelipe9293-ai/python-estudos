c48111c3fb29ecf5ce43e5fa16c96a8c18fd51a0
gilbertofelipe9293-ai <gilbertofelipe9293@gmail.com>
2026-08-22 14:40:25 -0300

    case 01 - funçãoes
    case de estudo usando funçãoes em phyton

--- case 01 funcaoes /funcaoes.py
+++ case 01 funcaoes /funcaoes.py
@@ -1,1 +1,18 @@
 
+def classificar_preco(preco):
+    if preco >= 100:
+        return "preco caro"
+
+    elif preco >= 50:
+        return "preco normal"
+
+    else:
+        return "barato"
+
+
+precos = [30, 80, 120, 45, 200]
+
+for preco in precos:
+    resulto = classificar_preco(preco)
+    print(resulto)
+     
