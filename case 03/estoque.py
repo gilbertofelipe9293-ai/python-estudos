24e843488e6d7eb2bad1c6ac58bb2591baaf29b3
gilbertofelipe9293-ai <gilbertofelipe9293@gmail.com>
2026-08-30 08:43:25 -0300

    Update estoque.py

--- case 03/estoque.py
+++ case 03/estoque.py
@@ -1,1 +1,35 @@
 
+estoque = {
+    "Arroz": {
+        "quantidade": 10,
+        "preco": 20
+    },
+    "Feijao": {
+        "quantidade": 5,
+        "preco": 8
+    },
+    "Milho": {
+        "quantidade": 0,
+        "preco": 6
+    }
+}
+
+
+def verificar(produto):
+
+    if produto in estoque:
+
+        quantidade = estoque[produto]["quantidade"]
+        preco = estoque[produto]["preco"]
+
+        return produto, quantidade, preco
+
+    else:
+        return "Produto não localizado"
+
+
+produto = input("Busque um produto: ")
+
+resultado = verificar(produto)
+
+print(resultado)
